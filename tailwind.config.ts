cb05e5d7c0e7790c6d9d35bcd5b963338a69f463
Robson Santos <robsonlsantos16@gmail.com>
2025-11-08 06:24:57 -0300

    hero section

---
+++ tailwind.config.ts
@@ -0,0 +1,76 @@
+import type { Config } from 'tailwindcss';
+
+
+export default {
+    darkMode: ['class'],
+    content: [
+    './src/pages/**/*.{js,jsx,tsx,mdx}',
+    './src/components/**/*.{js,jsx,tsx,mdx}',
+    './src/app/**/*.{js,jsx,tsx,mdx}',
+  ],
+  theme: {
+	container: {
+		center: true,
+		padding: "2rem",
+		screens: {
+			"2xl": "1200px",
+		},
+	},
+  	extend: {
+		  colors: {
+			  white: '#ffffff',
+			  blue: {
+				100: '#7ea4d7',
+				200: '#2c85fc',
+				300: '#2266c1',
+				400: '#0d284c',
+			},
+			cyan: {
+				100: '#2debfc',
+				200: '#187d86',
+				300: '#0e474c',
+			},
+			gray: {
+				100: '#f9fafc',
+				200: '#d3d5d9',
+				300: '#93979f',
+				400: '#20242c',
+				500: '#16181d',
+				600: '#14161a',
+				700: '#101216',
+				800: '#0b0c0f',
+			},
+		},
+		borderRadius: {
+			lg: '0.5rem',
+			md: 'calc(0.5rem - 2px)',
+			sm: 'calc(0.5rem - 4px)'
+		},
+		fontFamily: {
+			sans: ['PT Sans Caption', 'sans-serif'],
+			inter: ['Inter', 'sans-serif'],
+		},
+		fontSize: {
+        // Headings - PT Sans Caption
+        'heading-hg': ['40px', { lineHeight: '120%', fontWeight: '700' }],
+        'heading-xl': ['32px', { lineHeight: '120%', fontWeight: '700' }],
+        'heading-lg': ['28px', { lineHeight: '120%', fontWeight: '700' }],
+        'heading-md': ['24px', { lineHeight: '120%', fontWeight: '700' }],
+        'heading-sm': ['20px', { lineHeight: '120%', fontWeight: '700' }],
+        'heading-xs': ['16px', { lineHeight: '120%', fontWeight: '700' }],
+
+        // Body - Inter
+        'body-md': ['16px', { lineHeight: '150%', fontWeight: '400' }],
+        'body-sm': ['14px', { lineHeight: '150%', fontWeight: '400' }],
+        'body-xs': ['12px', { lineHeight: '150%', fontWeight: '400' }],
+
+        // Action - Inter
+        'action-md': ['16px', { lineHeight: 'normal', fontWeight: '500' }],
+        'action-sm': ['14px', { lineHeight: 'normal', fontWeight: '500' }],
+      },
+	},
+  },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
