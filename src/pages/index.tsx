929e253c889cae60975209ca057d64065b6548bb
eumemomrx <113364863+eumemomrx@users.noreply.github.com>
2025-11-11 16:45:57 +0000

    add background images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import { HeroSection } from "@/components/hero-section";
+import { HeroSection } from "@/components/hero-section"
+import { FeatureSection } from "@/components/feature-section"
+import { SupportSection } from "@/components/support-section"
+import { CustomerStorySection } from "@/components/customer-story-section"
+import { CallToAction } from "@/components/call-to-action"
 
 export default function Home() {
   return (
     <>
       <article className="flex flex-col">
         <HeroSection />
+        <FeatureSection />
+        <SupportSection />
+        <CustomerStorySection />
+        <CallToAction />
       </article>
     </>
   );
