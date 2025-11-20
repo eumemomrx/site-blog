814bd52f95018be0a75bd2a2267ec2243f9686bf
Robson Santos <robsonlsantos16@gmail.com>
2025-11-20 07:17:41 -0300

    add search

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,10 @@ import type { Config } from 'tailwindcss';
 export default {
     darkMode: ['class'],
     content: [
-    './src/pages/**/*.{js,jsx,tsx,mdx}',
-    './src/components/**/*.{js,jsx,tsx,mdx}',
-	'./src/templates/**/*.{js,jsx,tsx,mdx}',
-    './src/app/**/*.{js,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+	'./src/templates/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
 	container: {
