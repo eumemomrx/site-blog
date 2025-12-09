841c7537a902881761d05ae0916f37b5925cfd39
Robson Santos <robsonlsantos16@gmail.com>
2025-12-09 14:43:26 -0300

    add blog list

---
+++ src/templates/blog/blog-list.tsx
@@ -0,0 +1,46 @@
+import { Search } from "@/components/search";
+import { useRouter } from "next/router";
+import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
+import { allPosts } from "contentlayer/generated";
+
+export function BlogList() {
+
+    const router = useRouter()
+    const query = router.query.q as string
+    const pageTitle = query ? `Resultados de busca para "${query}"` : 'Dicas e estratégias para impulsionar seu negócio'
+
+    const posts = allPosts
+
+    return (
+        <div className="flex flex-col py-24 flex-grow h-full">
+            <header className="pb-14">
+                <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+                    <div className="flex flex-col gap-4 md:px-0">
+                        <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">BLOG</span>
+                        <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">{pageTitle}</h1>
+                    </div>
+                    <Search />
+                </div>
+            </header>
+            <PostGridCard>
+                {posts.map((post) => (
+
+                <PostCard 
+                    key={post._id}
+                    title={post.title}
+                    description={post.description}
+                    date= {new Date(post.date).toLocaleDateString("pt-BR")}
+                    slug={post.slug}
+                    image={post.image}
+                    author={{
+                        avatar: post.author.avatar,
+                        name: post.author.name,
+                    }}
+                />
+                ))}
+
+            </PostGridCard>
+        </div>
+    )
+}
